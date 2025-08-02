f1bf0b6bcfda0e16ce464cef455b42e72a8e37ce
Parham Forati <p.arhamforati838@gmail.com>
2025-08-02 14:34:14 +0330

    Update git_actions.py

--- src/core/git_actions.py
+++ src/core/git_actions.py
@@ -29,7 +29,6 @@ def get_zen_quotes():
 def commit_quote(repo, file_path='quotes.md'):
     try:
         file = repo.get_contents(file_path)
-        # content = file.decoded_content.decode()
         sha = file.sha
         file_exists = True
         current_content = file.decoded_content.decode()
@@ -69,30 +68,3 @@ if __name__ == "__main__":
     commit_quote(repo)
 
 
-
-# def commit_quote(repo, file_path='quotes.md'):
-#     try:
-#         file = repo.get_contents(file_path)
-#         # content = file.decoded_content.decode()
-#         sha = file.sha
-#         file_exists = True
-#     except UnknownObjectException:
-#         sha = None
-#         file_exists = False
-#     new_content = get_zen_quotes()
-#     commit_message = f"Updated Quotes with gitfather bot at {datetime.datetime.now()}"
-#     if file_exists:
-#         repo.update_file(
-#             path=file_path,
-#             message=commit_message,
-#             content=new_content,
-#             sha=sha,
-#             branch="main"
-#         )
-#     else:
-#         repo.create_file(
-#             path=file_path,
-#             message=commit_message,
-#             content=new_content,
-#             branch="main"
-#         )
